b9d2e13de8c4e562569b3a4f16af983ed1945168
Liam <liamjwatson@aol.com>
2026-02-01 20:47:00 +0000

    images added to main/home page

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 import Logo from "@/app/_components/Logo";
 import { FiSearch, FiShoppingBag, FiUser } from "react-icons/fi";
 import { FiMenu } from "react-icons/fi";
-import { useState, useRef } from "react";
+import { useState } from "react";
+import SearchBar from "./SearchBar";
 
 const mainNav = [
   { title: "Creations", link: "/creations" },
@@ -13,13 +14,17 @@ const mainNav = [
 ];
 
 const Navbar = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
   const [searchIsVisible, setSearchIsVisible] = useState(false);
   const [mobileMenu, setMobileMenu] = useState(false);
 
   function handleSearch() {
     setSearchIsVisible(!searchIsVisible);
-    inputRef.current?.focus();
+    // inputRef.current?.focus();
+  }
+
+  function closeSearch() {
+    setSearchIsVisible(false);
   }
   return (
     <>
@@ -68,14 +73,11 @@ const Navbar = () => {
           </ul>
 
           {/* Search bar */}
-          <div>
-            <input
-              ref={inputRef}
-              className={`${searchIsVisible ? "opacity-100 scale-x-100" : "opacity-0 scale-x-0"} absolute bg-light-primary-4 w-1/2 md:w-1/3 py-2 top-1/2 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2  transition-all duration-700 ease-out origin-right rounded-md px-4 shadow-sm shadow-light-primary-3`}
-              placeholder=".... "
-            />
-            <button className="">Search</button>
-          </div>
+
+          <SearchBar
+            searchIsVisible={searchIsVisible}
+            onCloseSearch={closeSearch}
+          />
         </div>
       </nav>
 
